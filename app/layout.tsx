775415f1da7e4caca6624903b96ec0aabceaf22b
Aayush1904 <asagrawal612@gmail.com>
2024-05-23 18:08:28 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import "../app/globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const IBMPlex = IBM_Plex_Sans({
+import "./globals.css";
+
+const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
   weight: ['400', '500', '600', '700'],
   variable: '--font-ibm-plex'
 });
 
 export const metadata: Metadata = {
-  title: "Imaginification",
-  description: "AI-powered Image Generator",
+  title: "Imaginify",
+  description: "AI-powered image generator",
 };
 
 export default function RootLayout({
@@ -21,7 +22,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#624cf5' }
+    }}>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
@@ -30,4 +33,3 @@ export default function RootLayout({
     </ClerkProvider>
   );
 }
-// IBM Plex is a typeface family designed by IBM. It's an open-source typeface that was created to be the global font for IBM — for both digital and print use. It includes a variety of styles such as sans-serif, serif, and monospace, making it suitable for a wide range of applications, from user interfaces to editorial design. IBM Plex is freely available for anyone to download, use, and modify under the SIL Open Font License.
